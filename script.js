8896c8fa4c28cdc620604dd7e2c835456d4e4717
Nivi327 <85208385+Nivi327@users.noreply.github.com>
2022-02-04 13:42:06 +0530

    Added all files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function catGenerator(){
+    var image=document.createElement('img');
+    var div=document.getElementById('cat-gen-div');
+    image.src="http://edgecats.net/";
+    div.appendChild(image);
+}
